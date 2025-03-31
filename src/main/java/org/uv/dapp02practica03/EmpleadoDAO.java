1018dfa662f7721a963be43b3b999c50f119028b
harlock024 <hadith024@gmail.com>
2025-03-31 13:17:40 -0600

    menu empleado added

--- src/main/java/org/uv/dapp02practica03/EmpleadoDAO.java
+++ src/main/java/org/uv/dapp02practica03/EmpleadoDAO.java
@@ -1,49 +1,105 @@
-    package org.uv.dapp02practica03;
+package org.uv.dapp02practica03;
 
-    import java.sql.Connection;
-    import java.sql.PreparedStatement;
-    import java.sql.ResultSet;
-    import java.sql.SQLException;
-    import java.util.ArrayList;
-    import java.util.List;
-    import java.util.logging.Level;
-    import java.util.logging.Logger;
-    import org.hibernate.Session;
-    import org.hibernate.SessionFactory;
-    import org.hibernate.Transaction;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
-    /**
-     *
-     * @author harlock024
-     */
-    public class EmpleadoDAO implements IDAOGeneral<EmpleadoPojo, Integer> {
+/**
+ *
+ * @author harlock024
+ */
+public class EmpleadoDAO implements IDAOGeneral<EmpleadoPojo, Long> {
 
-        @Override
-        public EmpleadoPojo create(EmpleadoPojo pojo) {
-            SessionFactory sf = HibernateUtil.getSessionFactory();
-            Session  session = sf.getCurrentSession();
-            Transaction transaction = session.beginTransaction();
-            session.save(pojo);
-            transaction.commit();
-            return pojo;
-        }
-        @Override
-        public EmpleadoPojo update(EmpleadoPojo pojo, Integer id) {
-                 throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-
-        }
-        @Override
-        public boolean delete(Integer id) {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-        }
-
-        @Override
-        public EmpleadoPojo fetch_by_id(Integer id) {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-        }
-
-        @Override
-        public List<EmpleadoPojo> fetch_all() {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-        }
+    @Override
+    public EmpleadoPojo create(EmpleadoPojo pojo) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(pojo);
+        transaction.commit();
+        return pojo;
     }
+
+    @Override
+    public EmpleadoPojo update(EmpleadoPojo pojo, Long id) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            EmpleadoPojo empleadoToEdit = session.get(EmpleadoPojo.class, id);
+            if (empleadoToEdit != null) {
+                empleadoToEdit.setName(pojo.getName());
+                empleadoToEdit.setAddress(pojo.getAddress());
+                empleadoToEdit.setPhone(pojo.getPhone());
+
+                session.update(empleadoToEdit);
+                transaction.commit();
+                return pojo;
+            }else{
+                transaction.commit();
+                return null;
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return pojo;
+    }
+    @Override
+    public boolean delete(Long id) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+
+        EmpleadoPojo pojo = session.get(EmpleadoPojo.class, id);
+        if (pojo != null) {
+            session.delete(pojo);
+            transaction.commit();
+            return true;
+        }
+        transaction.commit();
+
+        return false;
+    }
+
+    @Override
+    public EmpleadoPojo fetch_by_id(Long id) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+        EmpleadoPojo empleado = new EmpleadoPojo();
+        try {
+            empleado = session.get(EmpleadoPojo.class, id);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return empleado;
+    }
+
+    @Override
+    public List<EmpleadoPojo> fetch_all() {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+
+        List<EmpleadoPojo> empleados = new ArrayList<>();
+        try {
+//            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+//            CriteriaQuery<EmpleadoPojo> criteriaQuery = criteriaBuilder.createQuery(EmpleadoPojo.class);
+//            Root<EmpleadoPojo> root = criteriaQuery.from(EmpleadoPojo.class);
+//            criteriaQuery.select(root);
+//           empleados = session.createQuery(criteriaQuery).getResultList();
+            empleados = session.createQuery("From EmpleadoPojo", EmpleadoPojo.class).list();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return empleados;
+    }
+}
--- src/main/java/org/uv/dapp02practica03/EmpleadoPojo.java
+++ src/main/java/org/uv/dapp02practica03/EmpleadoPojo.java
@@ -26,10 +26,18 @@ public class EmpleadoPojo implements Serializable{
     private String address;
     private String phone;
 
+    public EmpleadoPojo( String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public EmpleadoPojo() {
+    }
+    
 
     
     
-
     public Long getId() {
         return id;
     }
